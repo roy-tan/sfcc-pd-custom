c40b2d1a37a290f52390763ad90ff06eb9d8aee3
Roy Tan <roy.tan@salesforce.com>
2019-07-09 15:07:25 +0200

    Fix for multiple add to cart
    Add to cart now works for different products

--- cartridges/plugin_pd_se_lib/cartridge/static/default/js/hotspot.js
+++ cartridges/plugin_pd_se_lib/cartridge/static/default/js/hotspot.js
@@ -1,16 +1,33 @@
-$(function() {
-    $("[id^=hotspot]")
-    .click(
+$(function () {
+    $("[id^=hotspot]").click(
         function () {
-            var hsDiv = "#hotspot-content-" + $(this).attr("data-pid");            
-            if ($(hsDiv).css("display") == "none")
-            {
+            var hsDiv = "#hotspot-content-" + $(this).attr("data-pid");
+            if ($(hsDiv).css("display") == "none") {
                 $(hsDiv).show();
             }
-            else
-            {
+            else {
                 $(hsDiv).hide();
             }
         }
     )
+    $("[id=product-detail]").click(
+        function() {            
+            $(this).attr('class', 'product-detail');
+        }
+    )
+
+    var waitForEl = function(selector, callback) {
+        if (jQuery(selector).length) {
+          callback();
+        } else {
+          setTimeout(function() {
+            waitForEl(selector, callback);
+          }, 100);
+        }
+    };
+
+    waitForEl(".add-to-cart-messages", function() {
+        $(".product-detail").removeClass("product-detail");
+    });
 });
+
