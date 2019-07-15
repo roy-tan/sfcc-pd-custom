f22ab4ad7f00a6978eb1fbf98ea8388dfe8238fc
Allard Buijen <abuijen@demandware.com>
2019-07-15 12:29:57 +0200

    Update gsQuestion.js

--- cartridges/plugin_pd_se_lib/cartridge/experience/components/guidedsell_layouts/gsQuestion.js
+++ cartridges/plugin_pd_se_lib/cartridge/experience/components/guidedsell_layouts/gsQuestion.js
@@ -3,6 +3,8 @@
 var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
 var PageRenderHelper = require('~/cartridge/experience/utilities/PageRenderHelper.js');
+var URLUtils = require('dw/web/URLUtils');
+var ImageTransformation = require('~/cartridge/experience/utilities/ImageTransformation.js');
 
 /**
  * Render logic for the layouts.1columnFullWidth.
@@ -15,8 +17,15 @@ module.exports.render = function (context) {
 
     if (content.question_text) {
         model.question_text = content.question_text;
-        model.question_number = content.question_number;
+        
         model.preference_name = content.preference_name;
+        /*model.question_banner = {
+            src: {
+                mobile  : ImageTransformation.url(content.question_banner.file, mobileImageTransformation),
+                desktop : ImageTransformation.url(content.question_banner.file, desktopImageTransformation)
+            },
+            alt         : content.question_banner.file.getAlt()
+        }*/
     }
 
 
