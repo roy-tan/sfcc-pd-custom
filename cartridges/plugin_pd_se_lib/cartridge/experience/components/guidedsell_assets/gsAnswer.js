a85e56efa71b3087c2cd4353b28d769d329ad1d4
Allard Buijen <abuijen@demandware.com>
2019-07-15 12:30:04 +0200

    Update gsAnswer.js

--- cartridges/plugin_pd_se_lib/cartridge/experience/components/guidedsell_assets/gsAnswer.js
+++ cartridges/plugin_pd_se_lib/cartridge/experience/components/guidedsell_assets/gsAnswer.js
@@ -2,6 +2,8 @@
 
 var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
+var URLUtils = require('dw/web/URLUtils');
+var ImageTransformation = require('~/cartridge/experience/utilities/ImageTransformation.js');
 
 /**
  * Render logic for answer component
@@ -13,6 +15,16 @@ module.exports.render = function (context) {
     if (content.answer_text) {
         model.answer_text = content.answer_text;
         model.answer_value = content.answer_value;
+        
+        var mobileImageTransformation = ImageTransformation.scale(content.answer_icon.metaData, 'mobile');
+        var desktopImageTransformation = ImageTransformation.scale(content.answer_icon.metaData, 'desktop');
+        model.answer_icon = {
+            src: {
+                mobile  : ImageTransformation.url(content.answer_icon.file, mobileImageTransformation),
+                desktop : ImageTransformation.url(content.answer_icon.file, desktopImageTransformation)
+            },
+            alt         : content.answer_icon.file.getAlt()
+        };
     }
 
     return new Template('experience/components/guidedsell_assets/gsAnswer').render(model).text;
