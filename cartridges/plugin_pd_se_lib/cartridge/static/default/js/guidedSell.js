685db2b608e263abd86eb1910a19671db17cf6fa
Allard Buijen <abuijen@demandware.com>
2019-07-16 15:08:49 +0200

    Update guidedSell.js

--- cartridges/plugin_pd_se_lib/cartridge/static/default/js/guidedSell.js
+++ cartridges/plugin_pd_se_lib/cartridge/static/default/js/guidedSell.js
@@ -12,34 +12,38 @@ $(document).ready( function() {
 				
 				if (length === indexplus) {
 					$(this).change( function () {
-						searchQuery = '';
-						varquestion = $('div[id*=question]').each( function () {
-							var questionName = $(this).find('input[name*=prefn]').attr('name');
-							var questionVal = $(this).find('input[name*=prefn]').val();
-							var answerName = $(this).find('input[name*=prefv]').attr('name');
-							var answerVal = $(this).find('input[name*=prefv]').val();
-							searchQuery = searchQuery + questionName + '=' + questionVal + '&' + answerName + '=' + answerVal + '&';
+						DoMyAjaxCall();
+						
+						$('[class*=gsQuestion]').each (function() {
+							$(this).change( function () {
+								DoMyAjaxCall();
+							});
 						});
-						//question.find()
-						var postUrl = "/on/demandware.store/Sites-RefArchGlobal-Site/en_GB/Search-ShowAjax?" + searchQuery;
-						$.get(postUrl)
-						.done(function( data ) {
-							var n = parseInt($(data).find('.result-count').text());
+						function DoMyAjaxCall() {
+							searchQuery = '';
+							varquestion = $('div[id*=question]').each( function () {
+								var questionName = $(this).find('input[name*=prefn]').attr('name');
+								var questionVal = $(this).find('input[name*=prefn]').val();
+								var answerName = $(this).find('input[name*=prefv]').attr('name');
+								var answerVal = $(this).find('input[name*=prefv]:checked').val();
+								searchQuery = searchQuery + questionName + '=' + questionVal + '&' + answerName + '=' + answerVal + '&';
+							});
+							//question.find()
+							var postUrl = "/on/demandware.store/Sites-RefArchGlobal-Site/en_GB/Search-ShowAjax?" + searchQuery;
+							$.get(postUrl)
+							.done(function( data ) {
+								var n = parseInt($(data).find('.result-count').text());
 
-							if (n > 0) {
-							var content = $( data ).find( ".tab-content.col-12" );
-							$( ".search-results .guidedSellResults" ).empty().append( content );
-							$('.refinement-bar').remove();
-							$('.search-results').show();
-							} else {
-								$( ".search-results .guidedSellResults" ).empty().append( "<h3>There were no results found!</h3>" );
-							}
-						  });
-						//alert($('#guidedSell').val());
-						/*$('#guidedSell').submit(function(event) {
-							event.preventDefault();
-							alert(this.action);
-					  	});*/
+								if (n > 0) {
+									var content = $( data ).find( ".tab-content.col-12" );
+									$( ".search-results .guidedSellResults" ).empty().append( content );
+									$('.refinement-bar').remove();
+									$('.search-results').show();
+								} else {
+									$( ".search-results .guidedSellResults" ).empty().append( "<h3>There were no results found!</h3>" );
+								}
+							});
+						}
 					});
 				}
 			});
