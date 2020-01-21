3912eb403b18d5ea35e20aa9660d7c260ca66d81
Roy Tan <roy.tan@salesforce.com>
2020-01-21 11:52:42 +0100

    Update assets.js

--- cartridges/plugin_pd_se_lib/cartridge/scripts/assets.js
+++ cartridges/plugin_pd_se_lib/cartridge/scripts/assets.js
@@ -1,28 +1,47 @@
 'use strict';
 
-/**
- * This script provides SFRA like static file registration - used as a fallback if storefront cartridge does not provide assets.js
- */
 var URLUtils = require('dw/web/URLUtils');
 
 var styles = [];
 var scripts = [];
 
+/**
+ * If the resource to add is not already in the resource array then add it to the array
+ * @param {Array} resourceArray - Either the scripts or styles array to which you want to add the resource src to.
+ * @param {string} src - URI of the resource to add
+ * @param {string} integrity - cryptographic hash of a resource
+ */
+function addResource(resourceArray, src, integrity) {
+    var result = {};
+    var exists = resourceArray.some(function (element) {
+        return element.src === src;
+    });
+
+    if (!exists) {
+        result.src = src;
+        if (integrity) {
+            result.integrity = integrity;
+        }
+
+        resourceArray.push(result);
+    }
+}
+
 module.exports = {
-    addCss: function (src) {
-        if (/((http(s)?:)?\/\/).*.css/.test(src) && styles.lastIndexOf(src) < 0) {
-            styles.push(src);
-        } else if (styles.lastIndexOf(URLUtils.staticURL(src).toString()) < 0) {
-            styles.push(URLUtils.staticURL(src).toString());
+    addCss: function (src, integrity) {
+        if (/((http(s)?:)?\/\/).*.css/.test(src)) {
+            addResource(styles, src, integrity);
+        } else {
+            addResource(styles, URLUtils.staticURL(src).toString(), integrity);
         }
     },
-    addJs: function (src) {
-        if (/((http(s)?:)?\/\/).*.js/.test(src) && scripts.lastIndexOf(src) < 0) {
-            scripts.push(src);
-        } else if (scripts.lastIndexOf(URLUtils.staticURL(src).toString()) < 0) {
-            scripts.push(URLUtils.staticURL(src).toString());
+    addJs: function (src, integrity) {
+        if (/((http(s)?:)?\/\/).*.js/.test(src)) {
+            addResource(scripts, src, integrity);
+        } else {
+            addResource(scripts, URLUtils.staticURL(src).toString(), integrity);
         }
     },
-    scripts : scripts,
-    styles  : styles
+    scripts: scripts,
+    styles: styles
 };
