3b167b735689ea18fd5906768339c087abd5fb5c
Roy Tan <roy.tan@roytan-ltm.internal.salesforce.com>
2020-05-01 13:13:30 +0200

    Layout Component - Display Top Visible
    Used to display a default component if other components that use targetting are not triggered

--- cartridges/plugin_pd_se_lib/cartridge/experience/components/se_custom_assets/titlescene.js
+++ cartridges/plugin_pd_se_lib/cartridge/experience/components/se_custom_assets/titlescene.js
@@ -23,21 +23,6 @@ module.exports.render = function (context) {
     var replacement = '<strong>$3</strong>';
 
     model.text_headline = content.text_headline.replace(expression, replacement);
-
-    if (content.image) {
-        var mobileImageTransformation = ImageTransformation.scale(content.image.metaData, 'mobile');
-        var desktopImageTransformation = ImageTransformation.scale(content.image.metaData, 'desktop');
-
-        model.image = {
-            src: {
-                mobile  : ImageTransformation.url(content.image.file, mobileImageTransformation),
-                desktop : ImageTransformation.url(content.image.file, desktopImageTransformation)
-            },
-            alt         : content.image.file.getAlt(),
-            focalPointX : content.image.focalPoint.x * 100 + '%',
-            focalPointY : content.image.focalPoint.y * 100 + '%'
-        };
-    }
-
+    model.image = ImageTransformation.getScaledImage(content.image);
     return new Template('experience/components/se_custom_assets/titlescene').render(model).text;
 };
